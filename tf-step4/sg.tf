fdbd15ea7aa18a1f63fca3515a6b20276bbd52dd
Hyeonji-Cha <viphj98@naver.com>
2026-07-29 11:15:07 +0900

    Feat: 프라이빗 서브넷 전용 라우트 테이블 연결

--- tf-step4/sg.tf
+++ tf-step4/sg.tf
@@ -55,6 +55,9 @@ resource "aws_security_group" "sg" {
   for_each    = local.security_groups
   name_prefix = "DE-AI-18-${each.key}-sg"
   description = "${upper(each.key)} Security Group"
+  tags = {
+    Name = "DE-AI-18-${each.key}-sg"
+  }
 
 
   # VPC지정
@@ -78,5 +81,34 @@ resource "aws_security_group" "sg" {
     to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
- 
+
+}
+
+############################################
+# Web -> Was 룰 적용, 포트 8000 오픈, 룰생성 선언
+############################################
+resource "aws_security_group_rule" "web-to-was" {
+  type = "ingress"
+  # 소스(web) 보안 그룸
+  source_security_group_id = aws_security_group.sg["web"].id
+  # 타겟(was) 보안 그룹
+  security_group_id = aws_security_group.sg["was"].id
+  from_port         = 8000
+  to_port           = 8000
+  protocol          = "tcp"
+}
+
+############################################
+# was -> DB 룰 적용, 포트 3306 오픈
+############################################
+resource "aws_security_group_rule" "was-to-to" {
+  type = "ingress"
+  # 소스(Was) 보안 그룸
+  source_security_group_id = aws_security_group.sg["was"].id
+  # 타겟(Db) 보안 그룹
+  security_group_id = aws_security_group.sg["db"].id
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+
 }
--- tf-step4/vpc.tf
+++ tf-step4/vpc.tf
@@ -50,3 +50,27 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 
 }
+
+# private 서브넷 구성
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.company-vpc-cha.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-northeast-2a"
+  tags = {
+    Name = "DE-AI-18-private-subnet"
+  }
+}
+
+# 라우트 테이블
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.company-vpc-cha.id
+  tags = {
+    Name = "DE-AI-18-cha-private-rt"
+  }
+}
+
+# 최종연결 (프라이빗 서브넷 -> 프라이빗전용 라우트테이블)
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
